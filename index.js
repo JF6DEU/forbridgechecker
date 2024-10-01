7a2f59d7b5b1f6bbd1caaba2ab93124c79e07ab8
JF6DEU <152010892+JF6DEU@users.noreply.github.com>
2024-10-01 18:55:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ if (token == "not set"){
     process.exit(1);
 }
 
-const serverid = "01HH6WE8A9RR6N9YVZNZAJRG92";
 const bridgebotid = "01J918J61M2VCS6X7R2XGF1JX5";
 
-const { Client, ServerMemberCollection} = require("revolt.js");
+const { Client, UserCollection} = require("revolt.js");
 let client = new Client();
 const http = require("http");
 
-client.on("ready", async () =>
-    console.info(`Logged in as ${client.user.username}!`)
-);
+client.on("ready", async () =>{
+    console.info(`Logged in as ${client.user.username}!`);
+    setInterval(async function(){
+        let bridgebot = (await new UserCollection(client).fetch(bridgebotid));
+        if (bridgebot == undefined){
+            console.error("ERROR! bridgebot cannot read");
+        }
+        status = bridgebot.online
+    }, 1000);
+});
 
 var status;
-setInterval(async function(){
-    let bridgebot = (await new ServerMemberCollection(client).fetch(serverid, bridgebotid)).user;
-    if (bridgebot == 'undefined'){
-        console.error("ERROR! bridgebot cannot read");
-    }
-    status = bridgebot.online
-}, 1000);
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {
